2d133ae6265f91a6c7993ab4cf12013a24fe3066
sanketshinde4082 <162864883+sanketshinde4082@users.noreply.github.com>
2025-11-18 12:34:30 +0530

    Update master1.sh

--- master1.sh
+++ master1.sh
@@ -5,18 +5,19 @@ set -e
 MASTER_IP=<MASTER_PRIVATE_IP>   # <-- CHANGE THIS
 
 echo "[1] Running kubeadm init..."
-sudo kubeadm init \
-  --pod-network-cidr=192.168.0.0/16 \
-  --control-plane-endpoint=$MASTER_IP
+
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+kubeadm join <MASTER-IP>:6443 --token <TOKEN> \
+    --discovery-token-ca-cert-hash sha256:<HASH>
 
 echo "[2] Setting kubectl access..."
+
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "[3] Installing Calico CNI..."
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/custom-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/calico.yaml
 
 echo "Waiting for Calico pods..."
 watch kubectl get pods -n calico-system
